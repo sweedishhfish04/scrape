177fe7f9eea27e5e0acedcd2d910d41543cbb3be
sweedishhfish04 <godziwa4@yahoo.com>
2018-09-15 15:24:59 -0500

    make it better

---
+++ db.js
@@ -0,0 +1,7 @@
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
